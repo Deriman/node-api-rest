c53175d188394add5542d0c32dead8236ed10a72
Jose C. Santos <zderimanz@hotmail.com>
2024-11-06 20:05:25 +0100

    Creamos ruta de ejemplo

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ const port = 3900
 app.use(cors())
 app.use(express.json())
 
+app.get('/probando', (req, res) => {
+    console.log("Ruta /probando")
+    res.send("Ruta probando")
+})
+
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`)
 })
