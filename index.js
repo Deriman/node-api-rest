675ec02ba6ab879a5627767807e3e9fcd94c9743
Jose C. Santos <zderimanz@hotmail.com>
2024-11-06 19:56:25 +0100

    Creamos el servidor utilizando express

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const connectionDB = require('./database/connection')
+const express = require("express")
+const cors = require("cors")
 
 connectionDB()
+
+const app = express()
+const port = 3900
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`)
+})
